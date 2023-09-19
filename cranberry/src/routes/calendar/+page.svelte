5689b03ac3d2f2e8b196c5bb8687f1f6fc26119a
Alexey Krikunov <alexey.krikunov@ringcentral.com>
2023-09-19 23:13:57 +0400

    steps to kit

---
+++ cranberry/src/routes/calendar/+page.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+    import EventCalendar from "../../lib/EventCalendar/EventCalendar.svelte";
+    import {
+        register,
+        init,
+        getLocaleFromNavigator,
+        locale,
+        locales,
+    } from "svelte-i18n";
+    import type { MOPCEvent } from "../../models/mopcEvent";
+    import { getEvents } from "../../services/event";
+
+    register("en", () => import("../../locales/en.json"));
+    register("ru", () => import("../../locales/ru.json"));
+
+    init({
+        fallbackLocale: "en",
+        initialLocale: getLocaleFromNavigator(),
+    });
+
+    let today: Date = new Date();
+    let yearToShow: number = today.getFullYear();
+    let monthToShow: number = today.getMonth();
+
+    let events: MOPCEvent[] = [];
+
+    async function updateEvents(year: number, month: number) {
+        const firstayOfMonth = new Date(year, month, 1);
+        const lastDayOfMonth = new Date(year, month + 1, 0);
+        events = await getEvents(firstayOfMonth, lastDayOfMonth);
+        console.log(events);
+    }
+</script>
+
+<main class="py-24 relative overflow-scroll">
+    <select bind:value={$locale}>
+        {#each $locales as locale}
+            <option value={locale}>{locale}</option>
+        {/each}
+    </select>
+
+    {#await updateEvents(yearToShow, monthToShow)}
+        Loading...
+    {:then _value}
+        <EventCalendar
+            showYear={yearToShow}
+            showMonth={monthToShow}
+            {events}
+            on:selectedDateChanged={async (e) => {
+                await updateEvents(e.detail.year, e.detail.month);
+            }}
+        />
+    {/await}
+</main>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.100);
+    }
+</style>
--- cranberry/svelte.config.js
+++ cranberry/svelte.config.js
@@ -1,7 +1,16 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
   preprocess: vitePreprocess(),
-}
+  vitePlugin: {
+    // This enables compile-time warnings to be
+    // visible in the learn.svelte.dev editor
+    onwarn: (warning, defaultHandler) => {
+      console.log('svelte:warnings:%s', JSON.stringify(warning));
+      defaultHandler(warning);
+    }
+  }
+};
+
+export default config;
