b36e7eae0ea466e46085785d2d356e85cc96857b
Alexey Krikunov <alexey.krikunov@ringcentral.com>
2023-09-28 21:44:50 +0400

    revork params parsing

--- goberry/router/events/handlers.go
+++ goberry/router/events/handlers.go
@@ -10,7 +10,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
-	helpers "github.com/Annaero/MOPC/goberry/helpers"
+	helpers "github.com/Annaero/MOPC/goberry/helpers/request"
 	models "github.com/Annaero/MOPC/goberry/models"
 )
 
@@ -22,23 +22,24 @@ func getEvents(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	startDateParam := r.URL.Query().Get("start_date")
-	endDateParam := r.URL.Query().Get("end_date")
-	if startDateParam == "" || endDateParam == "" {
+	eventsParamsNames := map[string]bool{"start_date": true, "end_date": true}
+	eventsParams, err := helpers.GetRequestParams(r, eventsParamsNames)
+	if err != nil {
 		var startDateMissingErr = helpers.RequestError{
 			Code:        400,
-			Description: "startDate is missing",
+			Description: err.Error(),
 		}
 		startDateMissingErr.Render(w, r)
 		return
 	}
 
-	startDate, startDateErr := time.Parse(eventDateLayout, startDateParam)
-	endDate, endDateErr := time.Parse(eventDateLayout, endDateParam)
+	startDate, startDateErr := time.Parse(eventDateLayout, eventsParams["start_date"])
+	endDate, endDateErr := time.Parse(eventDateLayout, eventsParams["end_date"])
 	if startDateErr != nil || endDateErr != nil {
 		var errorDescription = ""
 		if startDateErr != nil {
 			errorDescription += startDateErr.Error()
+			errorDescription += " "
 		}
 		if endDateErr != nil {
 			errorDescription += endDateErr.Error()
