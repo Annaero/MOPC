38a55caf54a7665df21b5897472cbcedbf53b639
Alexey Krikunov <alexey.krikunov@ringcentral.com>
2023-10-22 00:45:08 +0400

    prisma wars: programmers strikes back

--- cranberry/src/lib/components/eventcard/EventCard.svelte
+++ cranberry/src/lib/components/eventcard/EventCard.svelte
@@ -2,7 +2,7 @@
     import type { Event } from "$lib/models/event";
     import { _ } from "svelte-i18n";
 
-    export let morcEvent: Event;
+    export let event: Event;
     export let showInline: boolean = false;
 </script>
 
@@ -17,28 +17,42 @@
         <div
             class="flex flex-row justify-around p-4 md:flex-col md:items-center md:justify-center md:w-1/4"
         >
-            <div class="md:text-6xl">{morcEvent.startDate.getDate()}</div>
+            <div class="md:text-6xl">{event.startDate.getDate()}</div>
             <div class="md:text-xl">
-                {$_("date.month_names")[morcEvent.startDate.getMonth()]}
+                {$_("date.month_names")[event.startDate.getMonth()]}
             </div>
-            <div class="md:text-2xl">{morcEvent.startDate.getFullYear()}</div>
+            <div class="md:text-2xl">{event.startDate.getFullYear()}</div>
         </div>
         <div class="p-4 font-normal text-gray-800 md:w-3/4">
             <h1
                 class="mb-4 text-4xl font-bold leading-none tracking-tight text-gray-800"
             >
-                {morcEvent.name}
+                {event.name}
             </h1>
             <p class="leading-normal">
-                {morcEvent.description}
+                {event.description}
             </p>
             <div class="flex flex-row items-center mt-4 text-gray-700">
                 <div class="w-1/2 flex justify-end">
-                    <img
-                        src="https://collegefootballplayoff.com/images/section_logo.png"
-                        alt=""
-                        class="w-8"
-                    />
+                    <a href="/events/edit/{event.id}">
+                        <svg
+                            class="feather feather-edit"
+                            fill="none"
+                            height="24"
+                            stroke="currentColor"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            viewBox="0 0 24 24"
+                            width="24"
+                            xmlns="http://www.w3.org/2000/svg"
+                            ><path
+                                d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"
+                            /><path
+                                d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"
+                            />
+                        </svg>
+                    </a>
                 </div>
             </div>
         </div>
--- cranberry/src/routes/events/edit/[[id]]/+page.server.ts
+++ cranberry/src/routes/events/edit/[[id]]/+page.server.ts
@@ -1,12 +1,10 @@
-import { fail, error } from '@sveltejs/kit';
+import { fail, error, redirect } from '@sveltejs/kit';
 import { message, superValidate } from 'sveltekit-superforms/server';
 import { type Event, EventOptionalDefaultsSchema } from '$lib/models/event';
 import prisma from "$lib/db/prisma";
 import { EventType, Prisma } from "@prisma/client"
 
 import type { Actions, PageServerLoad } from '../$types';
-
-const schema = EventOptionalDefaultsSchema
 
 export const load: PageServerLoad = async ({ params }) => {
     let event: Event = null;
@@ -18,7 +16,7 @@ export const load: PageServerLoad = async ({ params }) => {
         if (!event) throw error(404);
     }
 
-    const form = await superValidate(event, schema);
+    const form = await superValidate(event, EventOptionalDefaultsSchema);
     const eventTypes = Object.values(EventType)
     console.log("FORM:", form)
 
@@ -30,7 +28,7 @@ export const actions: Actions = {
     default: async ({ request }) => {
         const data = await request.formData()
         console.log('POST:', data);
-        const form = await superValidate(data, schema);
+        const form = await superValidate(data, EventOptionalDefaultsSchema);
         console.log('VALIDATE RESULT:', form);
 
         // Convenient validation check:
@@ -40,22 +38,25 @@ export const actions: Actions = {
             return fail(400, { form });
         }
 
-        let upsertEvent: Event;
+        let event: Event;
+
         try {
-            let { id: _, ...updateEvent
-            } = form.data;
-            upsertEvent = await prisma.event.upsert({
-                where: { id: form.data.id },
-                update: updateEvent,
-                create: form.data
-            })
-            console.log("UPSERTED EVENT:", upsertEvent)
-            return { form };
+            if (!form.data.id) {
+                event = await prisma.event.create({ data: form.data })
+            }
+            else {
+                let { id: id, ...updateEvent
+                } = form.data;
+                event = await prisma.event.update({ where: { id: id }, data: updateEvent })
+            }
+            console.log("UPSERTED EVENT:", event)
         } catch (e) {
             console.error("THROW ERROR: ", e);
-            return message(form, JSON.stringify(e), {
+            return message(form, "Something went wrong :(", {
                 status: 500
             });
         }
+
+        throw redirect(300, "/events/" + event.id);
     }
 };
