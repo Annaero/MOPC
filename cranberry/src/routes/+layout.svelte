4edd13492b0896ab8f0b0814746ce219ab1c0888
Alexey Krikunov <alexey.krikunov@ringcentral.com>
2023-10-21 14:21:25 +0400

    MOPC button active

--- cranberry/src/routes/+layout.svelte
+++ cranberry/src/routes/+layout.svelte
@@ -20,7 +20,9 @@
 
 <div class="navbar bg-base-200">
     <div class="flex-1">
-        <a class="btn btn-ghost normal-case text-xl">MOPC</a>
+        <a class="btn btn-ghost normal-case text-xl" href="/events/calendar"
+            >MOPC</a
+        >
     </div>
 
     <div>
