5f435e6309b2e3b9454dafc90cbd804129e76bbb
Alexey Krikunov <alexey.krikunov@ringcentral.com>
2023-10-25 22:53:15 +0400

    minified version of the event card

--- cranberry/src/lib/components/datesheet/DateSheet.svelte
+++ cranberry/src/lib/components/datesheet/DateSheet.svelte
@@ -6,16 +6,16 @@
     export let endDate: Date = null;
 </script>
 
-<div class="flex flex-row justify-center w-96">
+<div class="flex flex-row justify-center w-full">
     <div
         class="flex flex-col border border-solid border-base-200 rounded-box w-1/4"
     >
         <div class="text-l bg-base-200 rounded-t-lg w-full">
-            {$_("date.month_names")[startDate.getMonth()]}
+            {startDate.getFullYear()}
         </div>
         <div class="text-2xl">{startDate.getDate()}</div>
         <div class="text-m">
-            {startDate.getFullYear()}
+            {$_("date.month_names")[startDate.getMonth()]}
         </div>
     </div>
     {#if endDate}
@@ -26,11 +26,11 @@
             class="flex flex-col place-items-center border border-solid border-base-200 rounded-box w-1/4"
         >
             <div class="text-l bg-base-200 rounded-t-lg w-full">
-                {$_("date.month_names")[endDate.getMonth()]}
+                {endDate.getFullYear()}
             </div>
             <div class="text-2xl">{endDate.getDate()}</div>
             <div class="text-m">
-                {endDate.getFullYear()}
+                {$_("date.month_names")[endDate.getMonth()]}
             </div>
         </div>
     {/if}
--- cranberry/src/lib/components/eventcard/EventCard.svelte
+++ cranberry/src/lib/components/eventcard/EventCard.svelte
@@ -3,22 +3,32 @@
     import { _ } from "svelte-i18n";
     import DateSheet from "../datesheet/DateSheet.svelte";
     import Icon from "@iconify/svelte";
+    import { goto } from "$app/navigation";
 
+    export let minified: boolean = true;
     export let event: Event;
+    export let redirect: string = `/events/id_${event.id}`;
 </script>
 
 <div
-    class="card max-w-md bg-base-100 shadow-xl border border-solid border-base-200"
+    class="card max-w-md bg-base-100 shadow-xl border border-solid border-base-200 hover:border-base-300 hoer:shadow-inner hover:cursor-default"
+    class:w-64={minified}
+    tabindex="0"
+    role="button"
+    on:keyup={() => goto(redirect)}
+    on:click={() => goto(redirect)}
 >
-    <div class="navbar pt-1 px-3">
-        <a class="navbar-start" href="/events/edit/id_{event.id}">
-            <Icon icon="carbon:edit" height="2em" width="2em" />
-        </a>
-        <div class="badge badge-success navbar-center">active</div>
-        <a class="navbar-end">
-            <Icon icon="carbon:star" height="2em" width="2em" />
-        </a>
-    </div>
+    {#if !minified}
+        <div class="navbar pt-1 px-3">
+            <a class="navbar-start" href="/events/edit/id_{event.id}">
+                <Icon icon="carbon:edit" height="2em" width="2em" />
+            </a>
+            <div class="badge badge-success navbar-center">active</div>
+            <a class="navbar-end">
+                <Icon icon="carbon:star" height="2em" width="2em" />
+            </a>
+        </div>
+    {/if}
     <figure class="px-3 pt-1">
         <img
             src="https://sun9-19.userapi.com/impf/tmr-b5CVibMPy3RiAm_biqiYSJiZEK2TthGeCg/rV0SjdKtsis.jpg?size=1920x768&quality=95&crop=0,639,2396,957&sign=3d064ce9265c2649cd8934b7e40edf99&type=cover_group"
@@ -26,8 +36,14 @@
             class="rounded-xl"
         />
     </figure>
-    <div class="card-body items-center text-center">
-        <h1 class="card-title">
+    <div
+        class="items-center text-center"
+        class:flex={minified}
+        class:flex-col={minified}
+        class:p-2={minified}
+        class:card-body={!minified}
+    >
+        <h1 class="card-title text-4xl" class:text-xl={minified}>
             {event.name}
         </h1>
         <div class="flex flex-row items-center text-center">
@@ -36,15 +52,25 @@
             {:else if event.type == "OFFLINE"}
                 <Icon icon="carbon:location" />
             {/if}
-            <p>{event.type}</p>
+            <p class="text-xs">{event.type}</p>
         </div>
-        <p class="pt-2">{event.description}</p>
-        <div class="divider pt-5"><p class="text-neutral">When</p></div>
-        <div>
+        <div class="pt-2 italic pb-2" class:line-clamp-4={minified}>
+            {#each event.description.split(/\r?\n/) as paragraph}
+                <p class="prose text-left pt-1 indent-5">
+                    {paragraph}
+                </p>
+            {/each}
+        </div>
+        {#if !minified}
+            <div class="divider"><p class="text-neutral">When</p></div>
+        {/if}
+        <div class="w-5/6">
             <DateSheet startDate={event.startDate} endDate={event.endDate} />
         </div>
-        <div class="card-actions pt-10">
-            <button class="btn btn-primary">Apply!</button>
-        </div>
+        {#if !minified}
+            <div class="card-actions pt-10">
+                <button class="btn btn-primary">Apply!</button>
+            </div>
+        {/if}
     </div>
 </div>
