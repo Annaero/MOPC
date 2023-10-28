2b2c2bb795e5566e7b8e8030ae0ebee15ad19280
Alexey Krikunov <alexey.krikunov@ringcentral.com>
2023-10-28 12:35:12 +0400

    add adapter

--- cranberry/svelte.config.js
+++ cranberry/svelte.config.js
@@ -1,4 +1,5 @@
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from "@sveltejs/adapter-vercel"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,6 +11,11 @@ const config = {
       console.log('svelte:warnings:%s', JSON.stringify(warning));
       defaultHandler(warning);
     }
+  },
+  kit: {
+    adapter: adapter({
+      // adapter options go here
+    })
   }
 };
 
