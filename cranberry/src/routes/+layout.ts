239fff08f357769b23e74c0b1e37ffc0cf738ef3
Alexey Krikunov <alexey.krikunov@ringcentral.com>
2023-10-29 13:28:20 +0400

    update package

--- cranberry/src/routes/+layout.ts
+++ cranberry/src/routes/+layout.ts
@@ -1,6 +1,10 @@
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY } from '$env/static/public'
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit'
 import type { Database } from '$lib/supabase'
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+ 
+inject({ mode: dev ? 'development' : 'production' });
 
 export const load = async ({ fetch, data, depends }) => {
     depends('supabase:auth')
